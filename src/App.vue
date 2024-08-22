263a55134cfe533f8d9a98e8bdf257776e0524f9
Emmanuel Rodrigues <emmanuelpachrod@gmail.com>
2024-08-23 00:04:44 +0300

    setup banner

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,17 @@
 <template>
-  <h1>My first Vue project!</h1>
+  <header class="banner">
+      <div class="apresentacao">
+        <img src="./assets/images/logo.svg" alt="Cookin up logo" class="logo">
+
+        <p class="cabecalho-lg frase-cabecalho">
+            <span class="texto-verde">A feast of ideas to</span>
+            wake up the chef in you!
+        </p>
+        <p class="subtitulo-lg">
+        Dive in to new recipes every day with ingredients that are within your reach!
+        </p>
+    </div>  
+    <img src="./assets/images/foto-banner.png" alt="Picture of a woman cooking with a glass bowl in hand. 
+    She's smiling and holding a phone between her head and shoulders." class="foto-banner">
+  </header>
 </template>
