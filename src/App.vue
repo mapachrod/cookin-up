58ebf6ff3a20c7bd2fe45c7c738e126ea4c981f3
Emmanuel Rodrigues <emmanuelpachrod@gmail.com>
2024-08-24 12:16:33 +0300

    Making banner
    html and styles sent to Banner.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
-<template>
-  <header class="banner">
-      <div class="apresentacao">
-        <img src="./assets/images/logo.svg" alt="Cookin up logo" class="logo">
+<script lang="ts">
+import Banner from './components/Banner.vue';
 
-        <p class="cabecalho-lg frase-cabecalho">
-            <span class="texto-verde">A feast of ideas to</span>
-            wake up the chef in you!
-        </p>
-        <p class="subtitulo-lg">
-        Dive in to new recipes every day with ingredients that are within your reach!
-        </p>
-    </div>  
-    <img src="./assets/images/foto-banner.png" alt="Picture of a woman cooking with a glass bowl in hand. 
-    She's smiling and holding a phone between her head and shoulders." class="foto-banner">
-  </header>
+export default {
+  components: { Banner } //when property is equal to its value, no need to repeat {Banner:Banner}
+}
+</script>
+
+<template>
+  <Banner />
 </template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Banner.vue
@@ -0,0 +1,71 @@
+<template>
+      <header class="banner">
+      <div class="apresentacao">
+        <img src="../assets/images/logo.svg" alt="Cookin up logo" class="logo">
+
+        <p class="cabecalho-lg frase-cabecalho">
+            <span class="texto-verde">A feast of ideas to</span>
+            wake up the chef in yourself!
+        </p>
+        <p class="subtitulo-lg">
+        Dive in to new recipes every day with ingredients that are within your reach!
+        </p>
+    </div>  
+    <img src="../assets/images/foto-banner.png" alt="Picture of a woman cooking with a glass bowl in hand. 
+    She's smiling and holding a phone between her head and shoulders." class="foto-banner">
+  </header>
+</template>
+
+<style scoped>
+.banner {
+  padding: 4rem 7.5rem;
+  color: var(--creme);
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  column-gap: 3.25rem;
+}
+
+.logo {
+  height: 4.5rem;
+  margin-bottom: 3rem;
+}
+
+.frase-cabecalho {
+  margin-bottom: 2rem;
+}
+
+.texto-verde {
+  color: var(--verde-medio, #3D6D4A);
+}
+
+.foto-banner {
+  width: 35rem;
+}
+
+@media only screen and (max-width: 1300px) {
+  .banner {
+    padding: 4rem 3.75rem;
+    flex-direction: column;
+    align-items: center;
+    gap: 1rem;
+  }
+
+  .logo {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+  }
+}
+
+@media only screen and (max-width: 767px) {
+  .banner {
+    padding: 4rem 1.5rem;
+  }
+
+  .foto-banner {
+    width: min(100%, 21.25rem);
+  }
+}
+</style>
